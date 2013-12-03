8a32434169c784fd2c577190d5e602c31af86a4a
Dani Gonzalez castillo <dax01_92@MacBook-Pro-de-Dani.local>
2013-12-03 15:11:33 +0100

    Initial Commit

---
+++ 1sillaparamibolso/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  1sillaparamibolso
+//
+//  Created by Dani Gonzalez castillo on 03/12/13.
+//  Copyright (c) 2013 Dani Gonzalez castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 1sillaparamibolso/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  1sillaparamibolso
+//
+//  Created by Dani Gonzalez castillo on 03/12/13.
+//  Copyright (c) 2013 Dani Gonzalez castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
